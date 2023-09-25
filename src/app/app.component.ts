037053c1135bddcb731cbbe4256deba957a37231
Jolte Teofil <jolteteofil@yahoo.com>
2023-09-25 14:23:02 +0300

    O1js

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,133 @@
 import { Component } from '@angular/core';
-import { SmartContract } from 'o1js';
+import {
+	AccountUpdate,
+	declareMethods,
+	declareState,
+	Field,
+	isReady, method,
+	Mina,
+	PrivateKey,
+	shutdown,
+	SmartContract, state,
+	State
+} from 'o1js';
 
-/* Commenting next line will stop the error */
-class TestContract extends SmartContract {}
+// DECORATORS VERSION (ORIGINAL)
+class Square extends SmartContract {
+	@state(Field) num = State<Field>();
+
+	override init() {
+		super.init();
+		this.num.set(Field(3));
+	}
+
+	@method update(square: Field) {
+		const currentState = this.num.get();
+		this.num.assertEquals(currentState);
+		square.assertEquals(currentState.mul(currentState));
+		this.num.set(square);
+	}
+}
+
+// NON-DECORATORS VERSION (SUGGESTED FOR JAVASCRIPT) - but same behaviour
+// class Square extends SmartContract {
+// 	num: State<Field> = State();
+//
+// 	override init() {
+// 		super.init();
+// 		this.num.set(Field(3));
+// 	}
+//
+// 	update(square: Field) {
+// 		const currentState = this.num.get();
+// 		this.num.assertEquals(currentState);
+// 		square.assertEquals(currentState.mul(currentState));
+// 		this.num.set(square);
+// 	}
+// }
+// declareMethods(Square, { update: [Field as any] });
+// declareState(Square, { num: Field as any });
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular-wasm';
+
+	async loadO1JS() {
+
+		await isReady;
+
+		console.log('o1js loaded');
+
+		const useProof = false;
+
+		const Local = Mina.LocalBlockchain({ proofsEnabled: useProof });
+		Mina.setActiveInstance(Local);
+		const { privateKey: deployerKey, publicKey: deployerAccount } = Local.testAccounts[0];
+		const { privateKey: senderKey, publicKey: senderAccount } = Local.testAccounts[1];
+
+// ----------------------------------------------------
+
+// Create a public/private key pair. The public key is your address and where you deploy the zkApp to
+		const zkAppPrivateKey = PrivateKey.random();
+		const zkAppAddress = zkAppPrivateKey.toPublicKey();
+
+// create an instance of Square - and deploy it to zkAppAddress
+		const zkAppInstance = new Square(zkAppAddress);
+		const deployTxn = await Mina.transaction(deployerAccount, () => {
+			AccountUpdate.fundNewAccount(deployerAccount);
+			zkAppInstance.deploy();
+		});
+		await deployTxn.sign([deployerKey, zkAppPrivateKey]).send();
+
+// get the initial state of Square after deployment
+		const num0 = zkAppInstance.num.get();
+		console.log('state after init:', num0.toString());
+
+// ----------------------------------------------------
+
+		const txn1 = await Mina.transaction(senderAccount, () => {
+			zkAppInstance.update(Field(9));
+		});
+		console.log('prooving');
+		await txn1.prove();
+		console.log('prooved');
+		await txn1.sign([senderKey]).send();
+
+		const num1 = zkAppInstance.num.get();
+		console.log('state after txn1:', num1.toString());
+
+// ----------------------------------------------------
+
+		try {
+			const txn2 = await Mina.transaction(senderAccount, () => {
+				zkAppInstance.update(Field(75));
+			});
+			await txn2.prove();
+			await txn2.sign([senderKey]).send();
+		} catch (ex: any) {
+			console.log(ex.message);
+		}
+		const num2 = zkAppInstance.num.get();
+		console.log('state after txn2:', num2.toString());
+
+// ----------------------------------------------------
+
+		const txn3 = await Mina.transaction(senderAccount, () => {
+			zkAppInstance.update(Field(81));
+		});
+		await txn3.prove();
+		await txn3.sign([senderKey]).send();
+
+		const num3 = zkAppInstance.num.get();
+		console.log('state after txn3:', num3.toString());
+
+// ----------------------------------------------------
+
+		console.log('Shutting down');
+
+		await shutdown();
+	}
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,17 @@
 module.exports = {
-    experiments: {
-        topLevelAwait: true,
-    }
+	experiments: {
+		topLevelAwait: true,
+	},
+	optimization: {
+		splitChunks: {
+			cacheGroups: {
+				o1js: {
+					test: /[\\/]node_modules[\\/](o1js)[\\/]/,
+					name: 'o1js',
+					chunks: 'all',
+				}
+			}
+		},
+		minimize: false,
+	},
 }
