168da72558c1186ec2e495486bebd9dc20c3f971
Jolte Teofil <jolteteofil@yahoo.com>
2023-09-26 12:45:15 +0300

    Splitted o1js

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -55,6 +55,11 @@ class Square extends SmartContract {
 })
 export class AppComponent {
 
+	constructor() {
+		(window as any)._mainWorker = '1';
+		(document as any)._mainWorker = '2';
+	}
+
 	async loadO1JS() {
 
 		await isReady;
@@ -64,70 +69,71 @@ export class AppComponent {
 		const useProof = false;
 
 		const Local = Mina.LocalBlockchain({ proofsEnabled: useProof });
-		Mina.setActiveInstance(Local);
-		const { privateKey: deployerKey, publicKey: deployerAccount } = Local.testAccounts[0];
-		const { privateKey: senderKey, publicKey: senderAccount } = Local.testAccounts[1];
-
-// ----------------------------------------------------
-
-// Create a public/private key pair. The public key is your address and where you deploy the zkApp to
-		const zkAppPrivateKey = PrivateKey.random();
-		const zkAppAddress = zkAppPrivateKey.toPublicKey();
-
-// create an instance of Square - and deploy it to zkAppAddress
-		const zkAppInstance = new Square(zkAppAddress);
-		const deployTxn = await Mina.transaction(deployerAccount, () => {
-			AccountUpdate.fundNewAccount(deployerAccount);
-			zkAppInstance.deploy();
-		});
-		await deployTxn.sign([deployerKey, zkAppPrivateKey]).send();
-
-// get the initial state of Square after deployment
-		const num0 = zkAppInstance.num.get();
-		console.log('state after init:', num0.toString());
-
-// ----------------------------------------------------
-
-		const txn1 = await Mina.transaction(senderAccount, () => {
-			zkAppInstance.update(Field(9));
-		});
-		console.log('prooving');
-		await txn1.prove();
-		console.log('prooved');
-		await txn1.sign([senderKey]).send();
-
-		const num1 = zkAppInstance.num.get();
-		console.log('state after txn1:', num1.toString());
-
-// ----------------------------------------------------
-
-		try {
-			const txn2 = await Mina.transaction(senderAccount, () => {
-				zkAppInstance.update(Field(75));
-			});
-			await txn2.prove();
-			await txn2.sign([senderKey]).send();
-		} catch (ex: any) {
-			console.log(ex.message);
-		}
-		const num2 = zkAppInstance.num.get();
-		console.log('state after txn2:', num2.toString());
-
-// ----------------------------------------------------
-
-		const txn3 = await Mina.transaction(senderAccount, () => {
-			zkAppInstance.update(Field(81));
-		});
-		await txn3.prove();
-		await txn3.sign([senderKey]).send();
-
-		const num3 = zkAppInstance.num.get();
-		console.log('state after txn3:', num3.toString());
-
-// ----------------------------------------------------
-
-		console.log('Shutting down');
-
-		await shutdown();
+// 		Mina.setActiveInstance(Local);
+// 		const { privateKey: deployerKey, publicKey: deployerAccount } = Local.testAccounts[0];
+// 		const { privateKey: senderKey, publicKey: senderAccount } = Local.testAccounts[1];
+//
+// // ----------------------------------------------------
+//
+// // Create a public/private key pair. The public key is your address and where you deploy the zkApp to
+// 		const zkAppPrivateKey = PrivateKey.random();
+// 		console.log(zkAppPrivateKey);
+// 		const zkAppAddress = zkAppPrivateKey.toPublicKey();
+//
+// // create an instance of Square - and deploy it to zkAppAddress
+// 		const zkAppInstance = new Square(zkAppAddress);
+// 		const deployTxn = await Mina.transaction(deployerAccount, () => {
+// 			AccountUpdate.fundNewAccount(deployerAccount);
+// 			zkAppInstance.deploy();
+// 		});
+// 		await deployTxn.sign([deployerKey, zkAppPrivateKey]).send();
+//
+// // get the initial state of Square after deployment
+// 		const num0 = zkAppInstance.num.get();
+// 		console.log('state after init:', num0.toString());
+//
+// // ----------------------------------------------------
+//
+// 		const txn1 = await Mina.transaction(senderAccount, () => {
+// 			zkAppInstance.update(Field(9));
+// 		});
+// 		console.log('prooving');
+// 		await txn1.prove();
+// 		console.log('prooved');
+// 		await txn1.sign([senderKey]).send();
+//
+// 		const num1 = zkAppInstance.num.get();
+// 		console.log('state after txn1:', num1.toString());
+//
+// // ----------------------------------------------------
+//
+// 		try {
+// 			const txn2 = await Mina.transaction(senderAccount, () => {
+// 				zkAppInstance.update(Field(75));
+// 			});
+// 			await txn2.prove();
+// 			await txn2.sign([senderKey]).send();
+// 		} catch (ex: any) {
+// 			console.log(ex.message);
+// 		}
+// 		const num2 = zkAppInstance.num.get();
+// 		console.log('state after txn2:', num2.toString());
+//
+// // ----------------------------------------------------
+//
+// 		const txn3 = await Mina.transaction(senderAccount, () => {
+// 			zkAppInstance.update(Field(81));
+// 		});
+// 		await txn3.prove();
+// 		await txn3.sign([senderKey]).send();
+//
+// 		const num3 = zkAppInstance.num.get();
+// 		console.log('state after txn3:', num3.toString());
+//
+// // ----------------------------------------------------
+//
+// 		console.log('Shutting down');
+//
+// 		await shutdown();
 	}
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,59 @@
+const path = require('path');
+
 module.exports = {
+	mode: 'development',
 	experiments: {
 		topLevelAwait: true,
 	},
+	entry: {
+		main: './src/main.ts',
+		o1js: 'node_modules/o1js/dist/web/index.js'
+	},
+	// entry: {
+	// 	o1js: /[\\/]node_modules[\\/](o1js)[\\/][\\/]dist[\\/]snarky.js/,
+	// },
+
+	// optimization: {
+	// 	splitChunks: {
+	// 		cacheGroups: {
+	// 			formComponents: {
+	// 				chunks: 'all',
+	// 				enforce: true,
+	// 				minChunks: 1,
+	// 				name: 'form-components',
+	// 				priority: 10,
+	// 				test: /[\\/]node_modules[\\/](o1js)[\\/]dist[\\/]web[\\/]snarky.js/,
+	// 			},
+	// 		},
+	// 	},
+	// },
 	optimization: {
-		splitChunks: {
-			cacheGroups: {
-				o1js: {
-					test: /[\\/]node_modules[\\/](o1js)[\\/]/,
-					name: 'o1js',
-					chunks: 'all',
-				}
-			}
-		},
+		// splitChunks: {
+		// 	cacheGroups: {
+		// 		o1js: {
+		// 			test: /[\\/]node_modules[\\/]/,
+		// 			name: 'vendor',
+		// 			chunks: 'all',
+		// 		},
+		// 	},
+		// },
 		minimize: false,
+	},
+	// optimization: {
+	// 	splitChunks: {
+	// 		chunks: 'all',
+	// 		cacheGroups: {
+	// 			o1js: {
+	// 				test: /[\\/]node_modules[\\/](o1js)[\\/]dist[\\/]web[\\/]index.js/,
+	// 				name: 'o1js',
+	// 				chunks: 'all',
+	// 			}
+	// 		}
+	// 	},
+	// },
+	output: {
+		wasmLoading: 'fetch',
+		filename: '[name].js',
+		path: path.resolve(__dirname, 'dist'),
 	},
 }
